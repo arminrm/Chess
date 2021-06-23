d0db89e9ca58c866c0bdaa12810eb99bc2106f8f
arminrm <68967290+arminrm@users.noreply.github.com>
2021-06-23 01:19:23 -0400

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,3 +1,4 @@
+from math import floor
 import arcade
 #maybe file for functions....
 
@@ -6,75 +7,105 @@ SCREEN_WIDTH = 800
 SCREEN_TITLE = "Starting Template"
 
 names = ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"]
-pieces = [] #include class
+pieces = []
+board = [[], [], [], [], [], [], [] ,[]]
+
+x_positions = []
+y_positions = []
 
 valid_choice = False  
 index = None
 
 turn = "WHITE"
-
 original_position = []  #include in class
-release_x = None
-release_y = None
+found = None
 
-def collision_detection():
-    
-    for i, piece in enumerate(pieces):       #potential bug
+def assignment():
+    board[int(floor(original_position[1] / 100))][int(floor(original_position[0] / 100))] = None
+    board[int(floor(pieces[index].y / 100))][int(floor(pieces[index].x / 100))] = pieces[index]
+
+#arraysname (row * total columns) + coloumn
+def collision_detection():   #backwards....
+
+    if original_position[1] > pieces[index].y:
+        y = -1
+    else:
+        y = 1
+
+    if original_position[0] > pieces[index].x:
+        x = -1
+    else:
+        x = 1
+
+    if pieces[index].x == original_position[0]:
+        for row in range(original_position[1], pieces[index].y, y * 100):
+            if board[int(floor(row / 100))][int(floor(original_position[0]/ 100))] != None and row != pieces[index].y and board[int(floor(row / 100))][int(floor(original_position[0]/ 100))] != pieces[index]:
+                [pieces[index].x, pieces[index].y] = original_position
+                return False
+    elif pieces[index].y == original_position[1]:
+        for col in range(original_position[0], pieces[index].x, x * 100):
+            if board[int(floor(original_position[1] / 100))][int(floor(col / 100))] != None and col != pieces[index].x and board[int(floor(original_position[1] / 100))][int(floor(col / 100))] != pieces[index]:
+                [pieces[index].x, pieces[index].y] = original_position
+                return False
+    else:
+        for i in range(0, abs(pieces[index].x - original_position[0]), 100):
+            if board[int(floor((original_position[1] + y * i) / 100))][int(floor((original_position[0] + x * i) / 100))] != None and board[int(floor((original_position[1] + (y * i)) / 100))][int(floor((original_position[0] + (x * i))/ 100))] != pieces[index]:
+                [pieces[index].x, pieces[index].y] = original_position
+                return False
+            
+
+    for i, piece in enumerate(pieces): 
         if i != index and piece.x == pieces[index].x and piece.y == pieces[index].y:
             if piece.colour == pieces[index].colour or piece.piece == "King":
-                pieces[index].x = original_position[0]
-                pieces[index].y = original_position[1]
+                [pieces[index].x, pieces[index].y] = original_position
                 return False
             else:
+                assignment()
                 pieces.pop(i)
                 return True
+
+    assignment()
     return True
 
-def valid_move():
+def valid_move():  #moves through king...
 
     global pieces, index, original_position
-    #loop through direction that piece has moved.
     if pieces[index].piece == "Rook":
-        if pieces[index].x == original_position[0]:
-            return collision_detection()
-        elif pieces[index].y == original_position[1]:
+        if pieces[index].x == original_position[0] or pieces[index].y == original_position[1]:
             return collision_detection()
         else:
-            pieces[index].x = original_position[0]
-            pieces[index].y = original_position[1]
+            [pieces[index].x, pieces[index].y] = original_position
+            return False
+    elif pieces[index].piece == "Pawn":
+        #if [pieces[index].x, pieces[index].y] == [original_position[0] + 100, original_position[1] + 100]:
+            #if board[int(floor(pieces[index].y/ 100))][int(floor(pieces[index].x / 100))] != None:
+                #return collision_detection()
+            #else:
+                #[pieces[index].x, pieces[index].y] = original_position
+                #return False
+        #elif [pieces[index].x, pieces[index].y] == [original_position[0], original_position[1] + 200]:
+        return collision_detection()
+    elif pieces[index].piece == "Bishop":
+        if pieces[index].x != original_position[0] and pieces[index].y != original_position[1] and abs(pieces[index].y - original_position[1]) == abs(pieces[index].x - original_position[0]):
+            return collision_detection()
+        else:
+            [pieces[index].x, pieces[index].y] = original_position
+            return False 
+    elif pieces[index].piece == "Queen": 
+        if pieces[index].x == original_position[0] or pieces[index].y == original_position[1] or abs(pieces[index].y - original_position[1]) == abs(pieces[index].x - original_position[0]):
+            return collision_detection()
+        else:
+            [pieces[index].x, pieces[index].y] = original_position
+            return False
+    elif pieces[index].piece == "Knight":
+        #make for-loop, with list containing moves made prior
+        if (pieces[index].x == (original_position[0] - 100) and pieces[index].y == original_position[1] - 200) or (pieces[index].x == (original_position[0] - 100) and pieces[index].y == original_position[1] + 200) or (pieces[index].x == (original_position[0] + 100) and pieces[index].y == original_position[1] + 200) or (pieces[index].x == (original_position[0] + 100) and pieces[index].y == original_position[1] - 200) or (pieces[index].x == (original_position[0] + 100) and pieces[index].y == original_position[1] - 200):
+            print("here")
+            return collision_detection()
+        else:
+            [pieces[index].x, pieces[index].y] = original_position
             return False
     return True
-
-def mouse_release(i, value): #the main purpose is to centre; also detects position change 
-
-    global original_position, index
-
-    if (value - (value % 50)) % 100 == 0:
-        if original_position[i] != value - (value % 50) + 50:
-            if i == 0:
-                pieces[index].x = value - (value % 50) + 50
-            elif i == 1:
-                pieces[index].y = value - (value % 50) + 50
-            return True
-        else:
-            if i == 0:
-                pieces[index].x = original_position[i] 
-            elif i == 1:
-                pieces[index].y = original_position[i]
-            return False
-    elif (value - (value % 50)) % 100 != 0:
-        if original_position[i] != value - (value % 50):
-            if i == 0:
-                pieces[index].x = value - (value % 50) 
-            elif i == 1:
-                pieces[index].y = value - (value % 50)
-            return True
-        else:
-            if i == 0:
-                pieces[index].x = original_position[i]
-            elif i == 1:
-                pieces[index].y = original_position[i]
-            return False
 
 class chess_piece():   #camel-case, capitalized
 
@@ -83,14 +114,15 @@ class chess_piece():   #camel-case, capitalized
         self.colour = colour
         self.x = x
         self.y = y
+        pieces.append(self)
     
     @classmethod
     def make_pieces(cls):
         for x, name in enumerate(names):
-            pieces.append(chess_piece (name, "WHITE", (100 * x) + 50, 50))
-            pieces.append(chess_piece (name, "BLACK", (100 * x) + 50, 750))
-            pieces.append(chess_piece ("Pawn", "WHITE", (100 * x) + 50, 150))
-            pieces.append(chess_piece ("Pawn", "BLACK", (100 * x) + 50, 650))
+         board[0].append(chess_piece(name, "WHITE", (100 * x) + 50, 50))
+         board[7].append(chess_piece(name, "BLACK", (100 * x) + 50, 750))
+         board[1].append(chess_piece("Pawn", "WHITE", (100 * x) + 50, 150))
+         board[6].append(chess_piece("Pawn", "BLACK", (100 * x) + 50, 650))
 
 class MyGame(arcade.Window):
 
@@ -102,6 +134,10 @@ class MyGame(arcade.Window):
 
     def setup(self):
 
+        for row in range(2, 6):
+            for col in range(8):
+                board[row].append(None)
+        
         chess_piece.make_pieces()
 
     def on_draw(self):
@@ -110,11 +146,14 @@ class MyGame(arcade.Window):
         #Print chess-board
         z = 0
         for y in range(0, 701, 100):
+            y_positions.append(y + 50)
             for x in range(0 + z, 701 + z, 100):
                 if x % 200 == 0:
                     arcade.draw_xywh_rectangle_filled(x - z, y, 100, 100, arcade.color.BLACK)
                 else:
                     arcade.draw_xywh_rectangle_filled(x - z, y, 100, 100, arcade.color.WHITE)
+                if y == 0:
+                    x_positions.append(x + 50)
             z += 100
 
         #print pieces
@@ -135,7 +174,7 @@ class MyGame(arcade.Window):
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
 
-        global pieces, index
+        global pieces, index, valid_choice
 
         if valid_choice == True:
             pieces[index].x = x
@@ -146,7 +185,6 @@ class MyGame(arcade.Window):
         global valid_choice, index, original_position
 
         original_position = []
-
         for i, piece in enumerate(pieces):
             if x in range(piece.x - 50, piece.x + 50) and y in range(piece.y - 50, piece.y + 50):
                 if piece.colour == turn:
@@ -156,12 +194,24 @@ class MyGame(arcade.Window):
             
     def on_mouse_release(self, x, y, button, key_modifiers):
         
-        global pieces, index, valid_choice, turn, release_x, release_y
+        global pieces, index, valid_choice, turn, found
 
-        if valid_choice == True:
-            release_x = mouse_release(0, x)
-            release_y = mouse_release(1, y)
-            if (release_x or release_y) and valid_move() is True:
+        found = False
+        
+        if valid_choice is True:
+            for row in range(8):
+                if y in range(y_positions[row] - 50, y_positions[row] + 50):
+                    for col in range(8):
+                        if x in range(x_positions[col] - 50, x_positions[col] + 50):
+                            pieces[index].x = x_positions[col]
+                            pieces[index].y = y_positions[row]
+                            found = True
+                            break
+
+                    if found:
+                        break
+  
+            if [pieces[index].x, pieces[index].y] != original_position and valid_move():
                 if turn == "WHITE":
                     turn = "BLACK"
                 else:
